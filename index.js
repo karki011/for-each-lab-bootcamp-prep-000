b7af81b8c7cdd362fe7d268729d38dc3e977c2f9
subash karki <skarki35205@hotmail.com>
2017-02-24 02:03:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function iterativeLog(){
-    
+function iterativeLog(array){
+    array.forEach((element, index) =>{
+        console.log(`${index}: ${element}`);
+    })
+}
+
+function iterate(callback){
+    let name = ["subash", "Lata","sunil"];
+    name.forEach(callback);
+    return name;
+}
+
+function doToArray(array, callback){
+    array.forEach(callback);
 }
